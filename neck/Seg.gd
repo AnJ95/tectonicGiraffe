ba4c881bd902125904784714735f962646df73b0
Angelo Aracri <angeloaracri@yahoo.de>
2020-07-12 01:06:59 +0200

    Added different segment types

--- neck/Seg.gd
+++ neck/Seg.gd
@@ -10,12 +10,16 @@ export(float) var angle_speed_right = 0.7
 export(float) var angle_delta_left = 0.0
 export(float) var angle_delta_right = 0.0
 
+export(float) var min_length = 50
+export(float) var max_length = 50
+export(float) var extend_contract_speed = 20
+
 export(float) var angle_wiggle_left = 0.0
 export(float) var angle_wiggle_right = 0.0
 export(float) var wiggle_period = 1.0
 
 export(float) var width = 10
-export(float) var length = 50
+export(float) var length_initial = 50
 
 
 
@@ -29,13 +33,15 @@ onready var circle = $Visual/Circle
 # STATE
 
 var show_start_anim = false
+var start_anim_done = false
 var giraffe
 var angle = 0
+var length = length_initial
 
 ############################################
 # RANDOM KEY MAPPING
 enum Action {
-    Left, Right
+    Left, Right, Extend, Contract
 }
 var keys = ["key_0", "key_1", "key_2", "key_3"]
 var antagonist_keys = {
@@ -59,8 +65,14 @@ func init_key_mapping():
 #        actions.erase(action)
 
     var key = keys[randi()%keys.size()]
+    keys.erase(key)
+    keys.erase(antagonist_keys[key])
     key_to_action_map[key] = Action.Left
     key_to_action_map[antagonist_keys[key]] = Action.Right
+    
+    key = keys[randi()%keys.size()]
+    key_to_action_map[key] = Action.Extend
+    key_to_action_map[antagonist_keys[key]] = Action.Contract
 ############################################
 # LIFE CYCLE
 func init(giraffe, show_start_anim=false):
@@ -88,13 +100,15 @@ func _ready():
     circle.position.x = 0
     
     if show_start_anim:
-        print("1")
-        $Tween.interpolate_method(self, "set_length", 0, length, 1.0, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT)
+        $Tween.connect("tween_all_completed", self, "_on_start_anim_done")
+        $Tween.interpolate_method(self, "set_length", 0, length_initial, 1.8, Tween.TRANS_BOUNCE, Tween.EASE_IN_OUT)
         $Tween.start()
     else:
-        print("2")
-        set_length(length)
+        set_length(length_initial)
 
+func _on_start_anim_done():
+    start_anim_done = true
+    
 func set_length(new_length):
     length = new_length
     line.rect_size.y = length
@@ -115,8 +129,17 @@ func _process(delta):
         angle -= angle_speed_left * delta
     if actions.has(Action.Right):
         angle += angle_speed_right * delta
+    var last_length = length
+    if actions.has(Action.Contract):
+        length -= extend_contract_speed * delta
+    if actions.has(Action.Extend):
+        length += extend_contract_speed * delta
 
     angle = clamp(angle, angle_initial - angle_delta_left, angle_initial + angle_delta_right)
+    length = clamp(length, min_length, max_length)
+    
+    if (!show_start_anim or start_anim_done) and last_length != length:
+        set_length(length)
     
     
     var wiggle = sin((2*PI) * giraffe.time / wiggle_period)
@@ -129,20 +152,34 @@ func _process(delta):
 func randomize_properties():
     angle_initial = rand_range(-0.3, 0.3)
     
-    angle_delta_left = rand_range(1.0, 2.2)
-    if randi()%100 < 20: angle_delta_left = 0
+    match randi() % 10:
+        # ANGLE
+        0: continue
+        1: continue
+        2: continue
+        3: continue
+        4: continue
+        5: continue
+        6:
+            angle_delta_left = rand_range(1.0, 2.2)
+            angle_delta_right = rand_range(1.0, 2.2)
+            angle_speed_left = rand_range(0.4, 0.7)
+            angle_speed_right = rand_range(0.4, 0.7)
+        # CRAZY ANGLE
+        7:
+            angle_delta_left = rand_range(PI, 2*PI)
+            angle_delta_right = rand_range(PI, 2*PI)
+            angle_speed_left = rand_range(0.8, 1.5)
+            angle_speed_right = rand_range(0.8, 1.5)
+        # EXTEND, CONTRACT
+        8: continue
+        9:
+            min_length = rand_range(length-25, length-15)
+            max_length = rand_range(length+15, length+25)
+            extend_contract_speed = rand_range(30, 60)
     
-    angle_delta_right = rand_range(1.0, 2.2)
-    if randi()%100 < 20: angle_delta_right = 0
-    
-    angle_speed_left = rand_range(0.25, 0.5)
-    if randi()%100 < 10: angle_speed_left = rand_range(1.5, 2.0)
-    
-    angle_speed_right = rand_range(0.25, 0.5)
-    if randi()%100 < 10: angle_speed_right = rand_range(1.5, 2.0)
-    
-    angle_wiggle_left = rand_range(0.1, 0.15)
-    angle_wiggle_right = rand_range(0.1, 0.15)
+    angle_wiggle_left = rand_range(0.05, 0.1)
+    angle_wiggle_right = rand_range(0.05, 0.1)
     wiggle_period = rand_range(0.6, 2.0)
     
 func get_next_seg_local_pos():
@@ -150,12 +187,17 @@ func get_next_seg_local_pos():
     
 
 var stored_rotation
+var stored_length
 func set_angle_random():
     var a_min = angle_initial - angle_delta_left
     var a_max = angle_initial + angle_delta_right
     
     stored_rotation = rotation
     rotation = a_min + (a_max - a_min) * randf()
+    
+    stored_length = length
+    set_length(min_length + (max_length - min_length) * randf())
 
 func restore_angle():
     rotation = stored_rotation
+    set_length(stored_length)
