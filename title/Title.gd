e9a2a82060275a829d6e66b282b9c4edb6cd4d1f
Angelo Aracri <angeloaracri@yahoo.de>
2020-07-12 18:27:14 +0200

    Finalised Title screen => ship it

--- title/Title.gd
+++ title/Title.gd
@@ -4,4 +4,9 @@ func _ready():
     $AnimationPlayer.play("wiggle")
 
 func _on_TextureButton_pressed():
+    print(1)
+    $Particles2D.emitting = true
+    $Timer.start()
+
+func _on_Timer_timeout():
     get_tree().change_scene("res://main/Main.tscn")
